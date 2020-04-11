74b5a8c20dc44f22f733c05e90897c825f3a3637
Amir Khorsandi <khorsandi@me.com>
2020-04-11 12:06:07 +0200

    Remove pods and add a xcode framework project

---
+++ CollectionViewPagingLayout/CollectionViewPagingLayout.h
@@ -0,0 +1,15 @@
+//
+//  CollectionViewPagingLayout.h
+//  CollectionViewPagingLayout
+//
+//  Created by Amir on 11/04/2020.
+//  Copyright © 2020 Amir. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CollectionViewPagingLayout.
+FOUNDATION_EXPORT double CollectionViewPagingLayoutVersionNumber;
+
+//! Project version string for CollectionViewPagingLayout.
+FOUNDATION_EXPORT const unsigned char CollectionViewPagingLayoutVersionString[];
