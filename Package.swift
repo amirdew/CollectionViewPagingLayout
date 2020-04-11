2bbe24b11ba2f25d1eb59b62f939d6d4a3abfdfe
Amir Khorsandi <khorsandi@me.com>
2020-04-11 10:41:10 +0200

    Add swift package

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "CollectionViewPagingLayout",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(
+            name: "CollectionViewPagingLayout",
+            targets: ["CollectionViewPagingLayout"]),
+    ],
+    targets: [
+        .target(
+            name: "CollectionViewPagingLayout",
+            dependencies: []),
+    ]
+)
