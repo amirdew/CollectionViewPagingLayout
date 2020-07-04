7d5205e2c4033199b50721e03dfb4a12b4757750
Amir Khorsandi <a.khorsandi@innovattic.com>
2020-07-04 18:10:03 +0200

    Add Copy button and code type button

--- Samples/PagingLayoutSamples/Modules/LayoutDesigner/Code/LayoutDesignerCodePreviewViewController.swift
+++ Samples/PagingLayoutSamples/Modules/LayoutDesigner/Code/LayoutDesignerCodePreviewViewController.swift
@@ -20,6 +20,8 @@ class LayoutDesignerCodePreviewViewController: UIViewController {
         }
     }
     private weak var codeTextView: UITextView!
+    private weak var copyButton: UIButton!
+    private weak var codeModeSegmentedControl: UISegmentedControl!
     
     // MARK: UIViewController
     
@@ -32,13 +34,52 @@ class LayoutDesignerCodePreviewViewController: UIViewController {
     // MARK: Private functions
     
     private func configureViews() {
+        configureTextView()
+        configureCopyButton()
+        configureCodeTypeSegmentedControl()
+    }
+    
+    private func configureCopyButton() {
+        let button = UIButton()
+        button.setImage(UIImage(named: "copyButton"), for: .normal)
+        button.addTarget(self, action: #selector(copyButtonTouched), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        copyButton = button
+        button.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        button.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -25).isActive = true
+        button.setContentCompressionResistancePriority(.required, for: .vertical)
+    }
+    
+    private func configureCodeTypeSegmentedControl() {
+        let segmentedControl = UISegmentedControl()
+        segmentedControl.insertSegment(withTitle: "Full ViewController", at: 0, animated: false)
+        segmentedControl.insertSegment(withTitle: "Options variable", at: 0, animated: false)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        codeModeSegmentedControl = segmentedControl
+        segmentedControl.topAnchor.constraint(equalTo: copyButton.topAnchor).isActive = true
+        segmentedControl.rightAnchor.constraint(equalTo: copyButton.leftAnchor, constant: -10).isActive = true
+        segmentedControl.heightAnchor.constraint(equalTo: copyButton.heightAnchor).isActive = true
+        segmentedControl.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        segmentedControl.selectedSegmentTintColor = UIColor.white.withAlphaComponent(0.8)
+        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
+        segmentedControl.setTitleTextAttributes([.foregroundColor: UIColor.black.withAlphaComponent(0.6)], for: .selected)
+        segmentedControl.selectedSegmentIndex = 0
+        
+    }
+    
+    @objc private func copyButtonTouched() {
+        
+    }
+    
+    private func configureTextView() {
         let codeTextView = UITextView()
         codeTextView.backgroundColor = .clear
         codeTextView.isEditable = false
-        view.fill(with: codeTextView, edges: .init(top: 20, left: 20, bottom: -20, right: -20))
+        view.fill(with: codeTextView, edges: .init(top: 100, left: 20, bottom: -20, right: -20))
         self.codeTextView = codeTextView
     }
-    
     private func refreshViews() {
         codeTextView.attributedText = viewModel?.getHighlightedText()
     }
