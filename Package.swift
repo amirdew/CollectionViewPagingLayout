720ed9f48411162735aeee55e6562771c3e38a84
Amir Khorsandi <amir.khorsandi@wetransfer.com>
2021-11-17 20:54:27 +0100

    Set target to iOS 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CollectionViewPagingLayout",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v13)
     ],
     products: [
         .library(
