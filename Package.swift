ea8bb42836631e2e30fe20cc753b51ed0d49bedc
Amir Khorsandi <amir.khorsandi@wetransfer.com>
2021-11-17 22:10:36 +0100

    Revert min ios to 10

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CollectionViewPagingLayout",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v10)
     ],
     products: [
         .library(
